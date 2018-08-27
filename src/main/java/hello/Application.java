792527f50cdcf508ef4fb3dbd772c0f3ca8b85c4
zhangran <zhangran@hualala.com>
2018-08-27 11:48:20 +0800

    pure springboot

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,20 +3,11 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "hello")
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    /*@Bean
-    public FilterRegistrationBean testFilterRegistration(){
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(new FilterTest());
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setName("filterTest");
-        registrationBean.setOrder(1);
-        return registrationBean;
-    }*/
 }
